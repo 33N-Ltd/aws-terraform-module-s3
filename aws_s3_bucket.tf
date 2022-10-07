1e7942ee06af49eea0d0c0bc8174718e121d97c7
Kav <kav@33n.co.uk>
2022-10-07 13:51:57 +0100

    slightly more complex

--- aws_s3_bucket.tf
+++ aws_s3_bucket.tf
@@ -3,68 +3,6 @@ resource "aws_s3_bucket" "bucket" {
   force_destroy = var.s3_bucket_force_destroy
 
   tags = merge(var.common_tags)
-
-  dynamic "lifecycle_rule" {
-    for_each = var.lifecycle_rule
-
-    content {
-      id                                     = lookup(lifecycle_rule.value, "id", null)
-      prefix                                 = lookup(lifecycle_rule.value, "prefix", null)
-      tags                                   = lookup(lifecycle_rule.value, "tags", null)
-      abort_incomplete_multipart_upload_days = lookup(lifecycle_rule.value, "abort_incomplete_multipart_upload_days", null)
-      enabled                                = lifecycle_rule.value.enabled
-
-      # Max 1 block - expiration
-      dynamic "expiration" {
-        for_each = length(keys(lookup(lifecycle_rule.value, "expiration", {}))) == 0 ? [] : [lookup(lifecycle_rule.value, "expiration", {})]
-
-        content {
-          date                         = lookup(expiration.value, "date", null)
-          days                         = lookup(expiration.value, "days", null)
-          expired_object_delete_marker = lookup(expiration.value, "expired_object_delete_marker", null)
-        }
-      }
-
-      # Several blocks - transition
-      dynamic "transition" {
-        for_each = lookup(lifecycle_rule.value, "transition", [])
-
-        content {
-          date          = lookup(transition.value, "date", null)
-          days          = lookup(transition.value, "days", null)
-          storage_class = transition.value.storage_class
-        }
-      }
-
-      # Max 1 block - noncurrent_version_expiration
-      dynamic "noncurrent_version_expiration" {
-        for_each = length(keys(lookup(lifecycle_rule.value, "noncurrent_version_expiration", {}))) == 0 ? [] : [lookup(lifecycle_rule.value, "noncurrent_version_expiration", {})]
-
-        content {
-          days = lookup(noncurrent_version_expiration.value, "days", null)
-        }
-      }
-
-      # Several blocks - noncurrent_version_transition
-      dynamic "noncurrent_version_transition" {
-        for_each = lookup(lifecycle_rule.value, "noncurrent_version_transition", [])
-
-        content {
-          days          = lookup(noncurrent_version_transition.value, "days", null)
-          storage_class = noncurrent_version_transition.value.storage_class
-        }
-      }
-    }
-  }
-
-  dynamic "logging" {
-    for_each = length(keys(var.logging)) == 0 ? [] : [var.logging]
-    content {
-      target_bucket = logging.value["target_bucket"]
-      target_prefix = lookup(logging.value, "target_prefix", var.target_prefix)
-    }
-  }
-
 }
 
 resource "aws_s3_bucket_public_access_block" "bucket_access" {
@@ -104,7 +42,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "bucket_encrpytion
   }
 }
 
-resource "aws_s3_bucket_cors_configuration" "bucket" {
+resource "aws_s3_bucket_cors_configuration" "bucket-cors" {
   bucket = aws_s3_bucket.bucket.id
 
   cors_rule {
@@ -113,5 +51,70 @@ resource "aws_s3_bucket_cors_configuration" "bucket" {
     allowed_origins = lookup(var.cors_rule, "allowed_origins", null)
     expose_headers  = lookup(var.cors_rule, "expose_headers", null)
     max_age_seconds = lookup(var.cors_rule, "max_age_seconds", null)
+  }
+}
+
+resource "aws_s3_bucket_logging" "bucket-logs" {
+  bucket = aws_s3_bucket.bucket.id
+
+  target_bucket = var.target_bucket
+  target_prefix = var.target_prefix
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "bucket-lifecycle" {
+  bucket = aws_s3_bucket.bucket.id
+
+  dynamic "rule" {
+
+    for_each = var.lifecycle_rule
+
+    content {
+      id                                     = lookup(rule.value, "id", null)
+      filter                                 = lookup(rule.value, "prefix", null)
+      tags                                   = lookup(rule.value, "tags", null)
+      abort_incomplete_multipart_upload_days = lookup(rule.value, "abort_incomplete_multipart_upload_days", null)
+      status                                 = rule.value.enabled ? "Enabled" : "Disabled"
+
+      # Max 1 block - expiration
+      dynamic "expiration" {
+        for_each = length(keys(lookup(rule.value, "expiration", {}))) == 0 ? [] : [lookup(rule.value, "expiration", {})]
+
+        content {
+          date                         = lookup(expiration.value, "date", null)
+          days                         = lookup(expiration.value, "days", null)
+          expired_object_delete_marker = lookup(expiration.value, "expired_object_delete_marker", null)
+        }
+      }
+
+      # Several blocks - transition
+      dynamic "transition" {
+        for_each = lookup(rule.value, "transition", [])
+
+        content {
+          date          = lookup(transition.value, "date", null)
+          days          = lookup(transition.value, "days", null)
+          storage_class = transition.value.storage_class
+        }
+      }
+
+      # Max 1 block - noncurrent_version_expiration
+      dynamic "noncurrent_version_expiration" {
+        for_each = length(keys(lookup(rule.value, "noncurrent_version_expiration", {}))) == 0 ? [] : [lookup(rule.value, "noncurrent_version_expiration", {})]
+
+        content {
+          days = lookup(noncurrent_version_expiration.value, "days", null)
+        }
+      }
+
+      # Several blocks - noncurrent_version_transition
+      dynamic "noncurrent_version_transition" {
+        for_each = lookup(rule.value, "noncurrent_version_transition", [])
+
+        content {
+          days          = lookup(noncurrent_version_transition.value, "days", null)
+          storage_class = noncurrent_version_transition.value.storage_class
+        }
+      }
+    }
   }
 }
