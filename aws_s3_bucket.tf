a613638e94fc007dc68d27599443e51d75db06bd
Kav <kav@33n.co.uk>
2022-10-10 12:22:15 +0100

    typo

--- aws_s3_bucket.tf
+++ aws_s3_bucket.tf
@@ -33,7 +33,7 @@ resource "aws_s3_bucket_versioning" "bucket-versioning" {
   }
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "bucket_encrpytion" {
+resource "aws_s3_bucket_server_side_encryption_configuration" "bucket_encryption" {
   bucket = aws_s3_bucket.bucket.id
   rule {
     apply_server_side_encryption_by_default {
